286b9f4e5d7e5ca1513ef5e9996f10d5182e0471
sridevkp <69075577+sridevkp@users.noreply.github.com>
2023-11-20 19:28:41 +0530

    added more functionality

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './Square.css'
 
-const Square = ({ index, fixed, parent, _val, highlight }) => {
+const Square = ({ index, fixed, parent, _val, invalid, inFocus, inRange }) => {
     const [ val, setVal ] = useState( _val )
+    
     return (<input 
-                className={"sudoko-square"+(fixed ? " fixed": "")+(highlight ? " warn" : "")} 
+                className={"sudoko-square"+(fixed ? " fixed": "")+(invalid ? " warn" : "")+(inFocus ? " focused" : "")+(inRange ? " check" : "")} 
                 value={val} 
                 type='number' 
                 min={1} max={9} step={1}
-                onFocus={ e => { if( !fixed ) e.target.select()} }
+                onFocus={ e => { 
+                    if( !fixed ){e.target.select()} 
+                    parent.focus( index, val)
+                }}
                 onChange={ e => { 
                     setVal( e.target.value )
                     parent.handleChange( index, e.target.value )
--- src/components/Sudoko.js
+++ src/components/Sudoko.js
@@ -5,9 +5,16 @@ import Square from './Square/Square'
 
 class Sudoko extends React.Component{
     data = Array(9*9).fill(null)
-    saved = [...this.data]
+    saved = Array(9*9).fill(null)
     state = {
-        highlights : []
+        invalidSquares : [],
+        focused : [],
+        range : []
+    }
+
+    constructor(){
+        super()
+        this.generatePuzzle()
     }
 
     generatePuzzle(){
@@ -26,8 +33,8 @@ class Sudoko extends React.Component{
             n, n, n,  5, n, n,  n, n, n 
         ]
         this.saved = [...this.data]
-        this.solve()
-        console.log( this.saved )
+        // this.solve()
+        // this.data = this.saved
         // for( i = 1; i < 10; i++){
         //     const index = Math.floor(Math.random() * 81)
 
@@ -37,14 +44,13 @@ class Sudoko extends React.Component{
     solve(){
         for( let i = 0; i < 81; i++ ){
             if( this.saved[i] != null ) continue 
-            console.log( i )
             for( let n = 1; n < 10; n++ ){
                 if( this.isPossibleAtIndex( i, n )){
-                    this.saved[i] = n
+                    this.data[i] = n
                     if( this.solve() ){
                         return true
                     }else{
-                        this.saved[i] = null
+                        this.data[i] = null
                     }
                 }
             }
@@ -52,9 +58,53 @@ class Sudoko extends React.Component{
         }
     }
 
+    focus( index, val ){
+        var focused = []
+        var range = []
+        for( let i = 0; i < 81; i++ ){
+            if( this.data[i] == val ){
+                focused.push( i )
+            }
+            if( this.inRange( i, index) ){
+                range.push( i )
+            }
+        }
+        // console.log( range )
+        this.setState({ focused, range })
+    }
+
+    check(){
+        var invalidSquares = []
+        for( let i = 0; i < 81; i++ ){
+            if( this.data[i] && ! this.isPossibleAtIndex( i, this.data[i] ) ){
+                invalidSquares.push(i)
+            }
+        }
+        this.setState({ invalidSquares })
+        if( invalidSquares.length == 0 ) return true
+        
+    }
+
+    getBoxCord( x, y ){
+        return { x : Math.floor(x/3), y : Math.floor(y/3) }
+    }
+
+    inRange( index1, index2 ){
+        const checkCord = this.indexToCord( index1 )
+        const { x, y } = this.indexToCord( index2 )
+    
+        const box1 = this.getBoxCord( checkCord.x, checkCord.y )
+        const box2 = this.getBoxCord( x, y )
+
+        if( checkCord.x == x || checkCord.y == y || (box1.x == box2.x && box1.y == box2.y)){
+            return true 
+        }
+        return false
+    }
+
     isPossibleAtIndex( index, num ){
-        const { row, col } = this.indexToCord(index)
-        return this.isPossible( row, col )
+        const { x, y } = this.indexToCord(index)
+        return this.isPossible( x, y, num )
     }
 
     isPossible( row, col, num ){ 
@@ -68,10 +118,10 @@ class Sudoko extends React.Component{
             const index = this.cordToIndex( row, j ) 
             if( this.data[index] == num ){ return false }
         }
-        //check boxes
+        // check boxes
         const offset = {
-            x : Math.floor( row /3 ),
-            y : Math.floor( col /3 )
+            x : Math.floor( row /3 ) * 3,
+            y : Math.floor( col /3 ) * 3
         }
         for( let i = 0; i < 3; i++ ){
             for( let j = 0; j < 3; j++){
@@ -100,26 +150,28 @@ class Sudoko extends React.Component{
     }
 
     handleChange( index, val ){
-        this.data[index] = val
+        this.data[index] = parseInt(val)
+        this.check()
     }
 
     render(){
-        this.generatePuzzle()
         return (
             <div className='container' >
-                <div className='background'></div>
                 {
                     [...this.data.keys()].map( (i) =>{ 
                         return <Square
                                     key = {i}
                                     index = {i}
-                                    fixed = { this.data[i] != null }
+                                    fixed = { this.saved[i] != null }
                                     parent = {this}
                                     _val = { this.data[i] == null ? "" : this.data[i] }
-                                    highlight = { i in this.state.highlights }
+                                    invalid = { this.state.invalidSquares.includes(i) }
+                                    inFocus = { this.state.focused.includes(i)}
+                                    inRange = { this.state.range.includes(i) }
                                 ></Square>
                         })
                 }
+                <div className='background'></div>
             </div>
         )
     }
